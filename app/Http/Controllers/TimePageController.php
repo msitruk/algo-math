12e386b4c42ddef06e91aabc3c5cb58a956040cd
msitruk <maxime.sitruk@feelandclic.com>
2017-02-01 14:35:29 +0100

    ajout des algo par sam

--- app/Http/Controllers/TimePageController.php
+++ app/Http/Controllers/TimePageController.php
@@ -16,10 +16,14 @@ class TimePageController extends Controller
 	   	//FIN DE LA PREPARATION DES DONNEES INITIALES
 
 	   	// -> EXEC DES TRIS <-//
-	   	$results = array();
-	   	$results[] = static::execTri('Tri à bulle', $datasBeforeTri, $inputs['nbexec']);
-	   	$results[] = static::execTri('Tri par insertion', $datasBeforeTri, $inputs['nbexec']);
-	   	$results[] = static::execTri('Tri par selection', $datasBeforeTri, $inputs['nbexec']);
+            $results = array();
+            $results[] = static::execTri('Tri à bulle', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri par insertion', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri par selection', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri par fusion', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri rapide', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri à peigne', $datasBeforeTri, $inputs['nbexec']);
+            $results[] = static::execTri('Tri de shell', $datasBeforeTri, $inputs['nbexec']);
 
 	   	//RETURN
 	   	return view('pages.time', compact('datasBeforeTri', 'results'));
@@ -135,6 +139,122 @@ class TimePageController extends Controller
 	    return $tableau;
 	}
 
+    public function triFusion($tableau)
+    {
+        if( count( $tableau ) <= 1 ) return;
+        else {
+            $tab1 = array();
+            $tab2 = array();
+
+            // Répartie les information dans 2 tableaux différent
+            for( $i = 0; $i < count( $tableau ); $i++) {
+                if( $i < ( count( $tableau ) ) / 2 )
+                    $tab1[] = $tableau[ $i ];
+                else
+                    $tab2[] = $tableau[ $i ];
+            }
+
+            // Appel la fonction tri récursivement
+            static::triFusion( $tab1 );
+            static::triFusion( $tab2 );
+
+            // Fusionne les petits tableaux en plus grand
+            static::fusionner( $tab1, $tab2, $tableau );
+        }
+        return $tableau;
+    }
+
+    public function fusionner ( $tab1, $tab2, &$tab )
+    {
+        $i = 0;
+        $i1 = $i2 = 0;
+        // Fusionne les petits tableaux dans le plus grand
+        while( $i1 < count( $tab1 ) && $i2 < count( $tab2 ) ) {
+            if( $tab1[ $i1 ] < $tab2[ $i2 ] ) // On compare ici
+                $tab[ $i ] = $tab1[ $i1++ ];
+            else
+                $tab[ $i ] = $tab2[ $i2++ ];
+            $i++;
+        }
+
+        // S'il reste des éléments dans un des 2 tableaux mais pas dans l'autre
+        while( $i1 < count( $tab1 ) ) {
+            $tab[ $i ] = $tab1[ $i1++ ];
+            $i++;
+        }
+        while( $i2 < count( $tab2 ) ) {
+            $tab[ $i ] = $tab2[ $i2++ ];
+            $i++;
+        }
+    }
+
+    public function triRapide($tableau)
+    {
+        if( count( $tableau ) < 2 ) {
+            return $tableau;
+        }
+        $left = $right = array( );
+        reset( $tableau );
+        $pivot_key  = key( $tableau );
+        $pivot  = array_shift( $tableau );
+        foreach( $tableau as $k => $v ) {
+            if( $v < $pivot )
+                $left[$k] = $v;
+            else
+                $right[$k] = $v;
+        }
+        return array_merge(static::triRapide($left), array($pivot_key => $pivot), static::triRapide($right));
+    }
+
+
+    public function triPeigne($tableau)
+    {
+        $gap = count($tableau);
+        $swap = true;
+        while ($gap > 1 || $swap){
+            if($gap > 1) $gap /= 1.25;
+            $swap = false;
+            $i = 0;
+            while($i+$gap < count($tableau)){
+                if($tableau[$i] > $tableau[$i+$gap]){
+                    list($tableau[$i], $tableau[$i+$gap]) = array($tableau[$i+$gap],$tableau[$i]);
+                    $swap = true;
+                }
+                $i++;
+            }
+        }
+        return $tableau;
+    }
+
+    public function triShell($tableau)
+    {
+        $gap = floor(count($tableau)/2);
+        while ($gap > 0) {
+            for ($i = 0; $i < count($tableau)-$gap; ++$i) {
+                $arrWithGapsKeys = array();
+                $arrWithGaps = array();
+                $loop = true;
+                $j = $i;
+                while ($loop) {
+                    if (isset($tableau[$j])) {
+                        $arrWithGapsKeys[] = (int)$j;
+                        $arrWithGaps[] = $tableau[$j];
+                        $j += $gap;
+                    } else {
+                        $loop = false;
+                    }
+                }
+                $arrWithGapsOrdered = static::triInsertion($arrWithGaps);
+                foreach ($arrWithGapsKeys as $key) {
+                    $arr[$key] = current($arrWithGapsOrdered);
+                    next($arrWithGapsOrdered);
+                }
+            }
+            $gap = floor($gap/2);
+        }
+        return $tableau;
+    }
+
 	public function execTri($nomDuTri, $datasInitial, $nbexec){
 	   	$time = array();
 	   	$datas = array();
@@ -154,8 +274,29 @@ class TimePageController extends Controller
 	   			$datas[] = static::triBulle($datasInitial);
 	    		$time[] = microtime(true) - $start;
 	   		}
+            if($nomDuTri == "Tri par fusion"){
+                $start = microtime(true);
+                $datas[] = static::triFusion($datasInitial);
+                $time[] = microtime(true) - $start;
+            }
+            if($nomDuTri == "Tri rapide"){
+                $start = microtime(true);
+                $datas[] = static::triRapide($datasInitial);
+                $time[] = microtime(true) - $start;
+            }
+            if($nomDuTri == "Tri à peigne"){
+                $start = microtime(true);
+                $datas[] = static::triPeigne($datasInitial);
+                $time[] = microtime(true) - $start;
+            }
+            if($nomDuTri == "Tri de shell"){
+                $start = microtime(true);
+                $datas[] = static::triShell($datasInitial);
+                $time[] = microtime(true) - $start;
+            }
 	   	}
-	   	$average = array_sum($time) / count($time); 
+	   	$average = array_sum($time) / count($time);
+        //$average = array_sum($time) / count($time;
 	   	return array('name' => $nomDuTri, 'data' => $datas[0], 'debugtime' => $time, 'Average time' => $average);
 	}
 }
